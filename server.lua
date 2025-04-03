874020ae60dd85503508b305f4be718c38e51eec
ProgramLuke <85005142+ExeLuke666@users.noreply.github.com>
2025-04-02 22:45:04 -0400

    NDCore & okokNotify addition

--- server.lua
+++ server.lua
@@ -2,16 +2,22 @@
 --- BadgerBankRobbery ---
 -------------------------
 --- Server ---
-robberyActive = false
+local robberyActive = false
+NDCore = exports["ND_Core"]
+
 RegisterNetEvent('BadgerBankRobbery:IsActive')
 AddEventHandler('BadgerBankRobbery:IsActive', function()
-	-- Check if active or not
-	if robberyActive then
-		-- One is active
-		TriggerClientEvent('BadgerBankRobbery:IsActive:Return', source, true)
+	TriggerClientEvent('BadgerBankRobbery:IsActive:Return', source, robberyActive)
+end)
+
+RegisterNetEvent("BadgerBankRobbery.getIsAllowed")
+AddEventHandler("BadgerBankRobbery.getIsAllowed", function()
+	local src = source
+	local player = NDCore.getPlayer(src)
+	if player and IsPlayerAceAllowed(src, "badgerbank.rob") then
+		TriggerClientEvent("BadgerBankRobbery.returnIsAllowed", src, true)
 	else
-		-- One is not active
-		TriggerClientEvent('BadgerBankRobbery:IsActive:Return', source, false)
+		TriggerClientEvent("BadgerBankRobbery.returnISAllowed", src, false)
 	end
 end)
 
@@ -19,9 +25,8 @@ RegisterNetEvent('BadgerBankRobbery:SetActive')
 AddEventHandler('BadgerBankRobbery:SetActive', function(bool)
 	robberyActive = bool
 	if bool then
-		Wait((1000 * 60 * config.robberyCooldown)) -- Wait 15 minutes
+		Wait(1000 * 60 * config.robberyCooldown)
 		robberyActive = false
-	end
 end)
 
 RegisterNetEvent('Print:PrintDebug')
@@ -29,7 +34,26 @@ AddEventHandler('Print:PrintDebug', function(msg)
 	print(msg)
 	TriggerClientEvent('chatMessage', -1, "^7[^1Badger's Scripts^7] ^1DEBUG ^7" .. msg)
 end)
+
 RegisterNetEvent('PrintBR:PrintMessage')
 AddEventHandler('PrintBR:PrintMessage', function(msg)
-	TriggerClientEvent('chatMessage', -1, msg)
+    TriggerClientEvent('chatMessage', -1, msg)
+end)
+
+RegisterNetEvent("Bank:Finished")
+AddEventHandler("Bank:Finished", function()
+	local src = source
+	local player = NDCore.getPlayer(src)
+	if player then
+		player.addMoney("cash", config.amount)
+	end
+end)
+
+RegisterNetEvent("Store:Finished")
+AddEventHandler("Store:Finished", function()
+	local src = source
+	local player = NDCore.getPlayer(src)
+	if player then
+		player.addMoney("cash", config.storeAmount)
+	end
 end)
